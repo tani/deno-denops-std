13ccffe93fbb85c817e837e55ffeb358cf3b5637
GitHub <noreply@github.com>
2021-10-01 00:45:49 +0000

    :package: Update deno dependencies

--- denops_std/deps.ts
+++ denops_std/deps.ts
@@ -3,6 +3,6 @@ export * as hash from "https://deno.land/std@0.109.0/hash/mod.ts";
 export * as path from "https://deno.land/std@0.109.0/path/mod.ts";
 export { deferred } from "https://deno.land/std@0.109.0/async/mod.ts";
 
-export * from "https://deno.land/x/denops_core@v2.0.0/mod.ts#^";
+export * from "https://deno.land/x/denops_core@v2.0.1/mod.ts#^";
 
-export * from "https://deno.land/x/unknownutil@v1.1.2/mod.ts#^";
+export * from "https://deno.land/x/unknownutil@v1.1.3/mod.ts#^";
